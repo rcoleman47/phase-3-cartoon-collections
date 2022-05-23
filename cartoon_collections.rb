658a9eb9b9af4d54abc4411b314ab2fde6090c00
ryanadams <rcoleman.adams@gmail.com>
2022-05-23 14:27:06 -0500

    commit tree 5d0087419d28c4f97b9f6798584f074903d889b9

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each.with_index {|val,index| puts "#{index+1}. #{val}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map {|value| value.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? {|value| value.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.find do |value| 
+    if array.include?(value)
+      value
+    else nil
+    end
+  end
 end
